7846d4831fb956632629aa465260b4ae4e960d01
Damon <zhuowangyi@163.com>
2020-03-06 13:58:14 +0800

    init

--- src/utils/testingEquipment.js
+++ src/utils/testingEquipment.js
@@ -4,5 +4,5 @@
 var is_mobi = navigator.userAgent.toLowerCase().match(/(ipod|iphone|android|coolpad|mmp|smartphone|midp|wap|xoom|symbian|j2me|blackberry|wince)/i) != null;
 if (is_mobi) {
 } else {
-    window.location.href="https://www.aitime.credit/#/home" 
+    window.location.href="https://www.heyfish.com/#/home" 
 }
